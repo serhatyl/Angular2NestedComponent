8fc35c8f7525c5ac80e8bdc908380f89cacfc811
Serhat Yılmaz <yilmaz.srhat@gmail.com>
2017-08-18 16:04:00 +0300

    Angular2 Interpolation
    Interpolation is meaning data binding.
    You can do data binding with 3 way
    1- One way data binding (Component to view template)
    2- One way data binding (View Template to Component)
    3- Two way data binding (Component to view template , view template to component)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   pageHeader = 'Employee Details';
+  imagePath = 'https://www.google.com.tr/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png';
+  firstName = 'Serhat';
+  lastName = 'Yılmaz';
+
+  getFullName() {
+    return this.firstName + ' ' + this.lastName;
+  }
+
 }
